dfc891ee375cd84512fdf10f78a1125c355aa083
MehediHasan23 <developer.mehedi23@gmail.com>
2022-07-21 11:15:41 +0600

    make some changes

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,14 @@
 /* dependencies */
 
 const Todo = require("../model/Todomodel");
+const User = require("../model/Usermodel");
 
 const indexController = async (req, res) => {
   try {
     const tasks = await Todo.find({ user: req.email });
-    res.render("index", { tasks });
+    const user = await User.findOne({ email: req.email });
+
+    res.render("index", { tasks, name: user.name });
   } catch (error) {
     throw error;
   }
